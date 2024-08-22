c0deb2ae1915ebd90028f48cbbf0890a0c1ae6b9
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-08-22 19:45:39 -0300

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         .then(data =>{
             console.log(data.message)
              if(data.message === 'Login succesful'){
-                window.location.replace("https://sistemacne-production.up.railway.app/session-status")
+                window.location.replace("https://sistemacne-production.up.railway.app/index.html")
                 return
             }else{
                 error.classList.add("active")
