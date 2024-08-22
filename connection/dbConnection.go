d1b1ea782cbbe5e73c4c617bdc8a6be4c23302a0
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-08-22 18:13:47 -0300

    Update dbConnection.go

--- connection/dbConnection.go
+++ connection/dbConnection.go
@@ -9,31 +9,6 @@ import (
 
 var DB *gorm.DB
 
-const DSN = "root:@tcp(127.0.0.1:3306)/Acneclinic?charset=utf8mb4&parseTime=True&loc=UTC"
-
-func ConnectionDB() {
-	var err error
-	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Base de Datos corriendo...")
-
-}
-
-/* package connection
-
-import (
-	"fmt"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
-var DB *gorm.DB
-
 const DSN = "root:mmPcIItbZImkXfBqVTUHknYepukLJNXn@tcp(junction.proxy.rlwy.net:59680)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 
 func ConnectionDB() {
@@ -46,4 +21,4 @@ func ConnectionDB() {
 
 	fmt.Println("Base de Datos corriendo...")
 
-} */
+} 
